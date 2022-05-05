a8a6973f2e7a91dfe50eea66fba4c54bd78cb3b5
[Prabhat244] <[prabhatsingh4873@gmail.com]>
2022-05-06 01:25:15 +0530

    Movie IMDB Backend Application

---
+++ Movie IMDB Backend Application/MoviesAPI/Data/Models/Producer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MoviesAPI.Data.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int producer_id { get; set; }
+        public string producer_name { get; set; }
+        public string company_name { get; set; }
+        public DateTime date_of_birth { get; set; }
+        public string gender { get; set; }
+    }
+}
